0f56f9deb676b6506027dcc17327acbaff3b8f5d
imb-bestrada <159048486+imb-bestrada@users.noreply.github.com>
2024-11-14 22:20:52 -0500

    14-11-20

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
     },
     {
         path: "/*",
-        element: <Navigate to="/home" replace={true} />
+        element: <Navigate to="/" replace={true} />
     },
 ]);
 
